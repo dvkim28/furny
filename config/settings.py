e532d19ac642693a484e6628e9790de09b014568
dmitriy kim <d.villarionovich@gmail.com>
2024-08-01 18:16:38 +0200

    cors updated

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "corsheaders"
+    "corsheaders",
     "rest_framework_simplejwt",
     "rest_framework",
     "user_service",
